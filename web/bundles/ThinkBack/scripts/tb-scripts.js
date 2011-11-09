4ee7456b706f94b120a4658777573a8ab76efdac
Simon Kerr <simon.p.kerr@gmail.com>
2011-11-09 18:16:40 +0000

    added jquery to base and created simple script to auto populate genres based on selected media type

---
+++ web/bundles/ThinkBack/scripts/tb-scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+var $allGenres = eval($("#mediaSelection_genres").val());
+var $selectedMediaGenres = $("select#mediaSelection_selectedMediaGenres");
+
+populateGenres($("#mediaSelection_mediaTypes").val());
+$("select#mediaSelection_mediaTypes").change(function(){
+    populateGenres($(this).val())
+});
+
+//when a media is selected, populate the genres
+function populateGenres(selectedMedia){
+    $selectedMediaGenres.empty();
+    $selectedMediaGenres.append("<option value=\"0\">All</option>");
+    
+    $.each($allGenres, function(i,genre){
+        if(genre.mediaType_id == selectedMedia){
+            $selectedMediaGenres.append("<option value=\""+ genre.id +"\">"+ genre.genreName +"</option>");
+            
+        }
+    });
+}
+
+});
+
+
+       
+
+
+
