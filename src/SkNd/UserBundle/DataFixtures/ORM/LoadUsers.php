da5d81f743e57ab23ca3656f7bd647ec3c0f4eab
Simon Kerr <simon.p.kerr@gmail.com>
2012-06-07 14:25:26 +0100

    updated some translations of phrases for the UserBundle, amended the base template to show flash messages in a list, tried to set up SwiftMailer but still not working, added fixtures class to load some sample users, finished tests for UserBundle controller

---
+++ src/SkNd/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * Original code Copyright (c) 2011 Simon Kerr
+ * LoadUsers loads fixtures to test the User functionality
+ * @author Simon Kerr
+ * @version 1.0
+ * to execute = php app/console doctrine:fixtures:load --fixtures=/path/to/fixture1 --fixtures=/path/to/fixture2 --append
+ */
+namespace SkNd\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use SkNd\UserBundle\Entity\User;
+
+class LoadUsers implements FixtureInterface, \Symfony\Component\DependencyInjection\ContainerAwareInterface {
+    
+    private $container; 
+    private $em;
+    private $userManager;
+    
+    //in order to get access to methods controlled by the container, it can be automatically injected using this method
+    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null){
+        $this->container = $container;
+        $this->em = $this->container->get('doctrine')->getEntityManager();
+        $this->userManager = $this->container->get('fos_user.user_manager');
+    }
+    
+    public function load(ObjectManager $manager){
+        //delete all records from the cached listings table first 
+        $q = $this->em->createQuery('DELETE from SkNd\UserBundle\Entity\User');
+        $q->execute();
+        
+        //3 sample users
+        $user = $this->userManager->createUser();
+        $user->setDateofbirth(new \DateTime("now"));
+        $user->setUsername('testuser');
+        $user->setEmail('test@test.com');
+        $user->setPlainPassword('testuser');
+        $user->setEnabled(true);
+        $this->userManager->updateUser($user, true);
+        //$manager->persist($user);
+        
+        $user = $this->userManager->createUser();
+        $user->setDateofbirth(new \DateTime("now"));
+        $user->setUsername('testuser2');
+        $user->setEmail('test2@test.com');
+        $user->setPlainPassword('testuser2');
+        $user->setEnabled(true);
+        $this->userManager->updateUser($user, true);
+        
+        $user = $this->userManager->createUser();
+        $user->setDateofbirth(new \DateTime("now"));
+        $user->setUsername('testuser3');
+        $user->setEmail('test3@test3.com');
+        $user->setPlainPassword('testuser3');
+        $user->setEnabled(true);
+        $this->userManager->updateUser($user, true);
+        
+    }
+       
+    
+}
+
+?>
--- src/SkNd/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ src/SkNd/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  * DefaultControllerTest for UserBundle tests all the 
  * functionality of the FOSUserBundle,
  * log in/out, register, update profile, change password etc
+ * the DataFixtures/ORM/LoadUsers fixtures file should be loaded first
  * @author Simon Kerr
  * @version 1.0
  */
@@ -21,7 +22,6 @@ class DefaultControllerTest extends WebTestCase
     public function setup(){
         $this->client = static::createClient();
         $this->client->followRedirects(true);
-        //$this->client->insulate();
     }
     
     //test all the routes    
@@ -83,6 +83,7 @@ class DefaultControllerTest extends WebTestCase
     
     public function testRegisterPostWithMissingUsernameShowsErrors(){
         $crawler = $this->client->request('GET', '/register');
+        //below line removes the html5 client side validation from the submit button to check server side validation
         $crawler->selectButton('Register')->addContent('formnovalidate="formnovalidate"');
         $form = $crawler->selectButton('Register')->form();
         $params = array(
@@ -108,6 +109,132 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('html:contains("The username is too short")')->count() > 0);
         $this->assertTrue($crawler->filter('html:contains("Please enter an email")')->count() > 0);
     }
+    
+    public function testResetPasswordWithInvalidCredentialsShowsErrors(){
+        $crawler = $this->client->request('GET', '/resetting/request');
+        $form = $crawler->selectButton('Reset password')->form();
+        $params = array(
+            'username' => 'bogus_username',
+        );
+       
+        $crawler = $this->client->submit($form, $params);
+        $this->assertTrue($crawler->filter('html:contains("does not exist.")')->count() > 0);
+    }
+    
+    //requires a user exist called testuser with a password of testuser
+    public function testUpdateProfileWithInvalidCredentialsShowsErrors(){
+        //log in first
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Login')->form();
+        $params = array(
+            '_username' => 'testuser',
+            '_password' => 'testuser',
+            
+        );
+        $crawler = $this->client->submit($form, $params);
+        $profileLink = $crawler->selectLink('Manage your account')->link();
+        $crawler = $this->client->click($profileLink);
+        
+        $crawler->selectButton('Update your profile')->addContent('formnovalidate="formnovalidate"');
+        $form = $crawler->selectButton('Update your profile')->form();
+        $params = array(
+            'fos_user_profile_form[user][username]' => '',
+            'fos_user_profile_form[user][email]'    => 'invalid_email',
+            'fos_user_profile_form[user][firstname]'=> 'u',
+            'fos_user_profile_form[current]'        => 'wrong_password',
+        );
+       
+        $crawler = $this->client->submit($form, $params);
+        $this->assertTrue($crawler->filter('html:contains("Please enter a username")')->count() > 0, "no username specified");
+        $this->assertTrue($crawler->filter('html:contains("The email is not valid")')->count() > 0, "invalid email given");
+        $this->assertTrue($crawler->filter('html:contains("This firstname is too short")')->count() > 0, "first name is too short");
+        $this->assertTrue($crawler->filter('html:contains("This password is invalid")')->count() > 0, "invalid password");
+        
+    }
+    
+    //requires a user exist called testuser with a password of testuser
+    public function testUpdateProfileWithCorrectCredentialsAndNewFirstNameShowsProfile(){
+        //log in first
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Login')->form();
+        $params = array(
+            '_username' => 'testuser',
+            '_password' => 'testuser',
+            
+        );
+        $crawler = $this->client->submit($form, $params);
+        $profileLink = $crawler->selectLink('Manage your account')->link();
+        $crawler = $this->client->click($profileLink);
+        
+        $crawler->selectButton('Update your profile')->addContent('formnovalidate="formnovalidate"');
+        $form = $crawler->selectButton('Update your profile')->form();
+        $params = array(
+            'fos_user_profile_form[user][firstname]'=> 'Simon',
+            'fos_user_profile_form[current]'        => 'testuser',
+        );
+       
+        $crawler = $this->client->submit($form, $params);
+        $this->assertTrue($crawler->filter('html:contains("Your profile has been updated")')->count() > 0, "profile successfully updated.");
+        
+    }
+    
+    public function testChangePasswordWithDifferentPasswordsShowsErrors(){
+         //log in first
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Login')->form();
+        $params = array(
+            '_username' => 'testuser',
+            '_password' => 'testuser',
+            
+        );
+        $crawler = $this->client->submit($form, $params);
+        
+        //go to the profile page
+        $crawler = $this->client->request('GET', '/change-password/change-password');
+        $form = $crawler->selectButton('Change password')->form();
+        $params = array(
+            'fos_user_change_password_form[current]' => 'testuser',
+            'fos_user_change_password_form[new][first]' => 'testuser_new',
+            'fos_user_change_password_form[new][second]' => 'testuser_invalid',
+            
+        );
+        $crawler = $this->client->submit($form, $params);
+        $this->assertTrue($crawler->filter('html:contains("Please enter a new password")')->count() > 0);
+        
+    }
+    
+    public function testChangePasswordWhenNotLoggedInRedirectsToLoginPage(){
+        $crawler = $this->client->request('GET', '/change-password/change-password');
+        
+        $this->assertTrue($crawler->selectButton("Login")->count() > 0, "redirected to log in screen");
+        
+    }
+    
+    
+    public function testChangePasswordWithCorrectDataShowsProfile(){
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Login')->form();
+        $params = array(
+            '_username' => 'testuser3',
+            '_password' => 'testuser3',
+            
+        );
+        $crawler = $this->client->submit($form, $params);
+        $crawler = $this->client->request('GET', '/change-password/change-password');
+        
+        $form = $crawler->selectButton('Change password')->form();
+        $params = array(
+            'fos_user_change_password_form[current]'        => 'testuser3',
+            'fos_user_change_password_form[new][first]'     => 'testuser3_new',
+            'fos_user_change_password_form[new][second]'    => 'testuser3_new',
+            
+        );
+        $crawler = $this->client->submit($form, $params);
+        
+        $this->assertTrue($crawler->filter('html:contains("The password has been changed")')->count() > 0, "Password changed.");
+        
+    }
+    
     
     
     
