1d57635b5802b98c43b17d537255ad00ce14bb62
Simon Kerr <simon.p.kerr@gmail.com>
2012-08-03 17:39:06 +0100

    - developed css/ styling - added social media sharing icons for facebook, twitter, pinterest and email - amended templates to make styling easier

---
+++ web/bundles/SkNd/scripts/share-scripts.js
@@ -0,0 +1,10 @@
+/*head section */
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
+
+/* page footer */
+<script>
+var options={ "publisher": "c4eb1bfb-3a57-45a0-a00e-98b89cdd16b0", "position": "left", "ad": { "visible": false, "openDelay": 5, "closeDelay": 0}, "chicklets": { "items": ["facebook", "twitter", "pinterest", "email", "sharethis"]}};
+var st_hover_widget = new sharethis.widgets.hoverbuttons(options);
+</script>
