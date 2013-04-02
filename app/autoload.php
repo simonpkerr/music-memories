8f6681411b852660a9e356f75333f644f23ca2e7
Simon Kerr <simon.p.kerr@gmail.com>
2013-04-02 11:14:59 +0100

    - upgraded project to symfony 2.2 - fixed issues with invalid entities, and deprecated calls in templates - begun integrating sonata user bundle but still needs work to integrate with already extended user entity.

--- app/autoload.php
+++ app/autoload.php
@@ -2,9 +2,9 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 //ini_set("include_path", ".:/usr/lib/php:/usr/local/lib/php:/home/simonker/public_html/SkNd/app/Resources/ZendFramework/library");
-//ini_set("memory_limit","32M");
 
 $loader = require __DIR__.'/../vendor/autoload.php';
+//$loader->add('Application', __DIR__);
 
 // intl
 if (!function_exists('intl_get_error_code')) {
@@ -13,9 +13,6 @@ if (!function_exists('intl_get_error_code')) {
     $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
-//$loader->registerPrefixFallbacks(array_merge(explode(';',get_include_path())));
-//the zend framework has been added to the include path in the php.ini file
-//$loader->registerPrefixFallbacks(array(__DIR__.'/Resources/ZendFramework/library'));
 require_once 'Zend/Loader.php';
 \Zend_Loader::loadClass('Zend_Gdata_YouTube');
 
